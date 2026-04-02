37ee7cd284434cf25ed9dc73f474798637c879f9
berturk25 <berturk25@ku.edu.tr>
2026-04-02 20:56:59 +0300

    completed

--- q1.py
+++ q1.py
@@ -1,5 +1,29 @@
 def longest_palindromic_substring(s):
-    """
-    Given a string find the longest palindromic substring
-    """
-    pass
+    if len(s) <2:
+        return ""
+    
+    longest_palindrome = ""
+
+    def extend_from_center(s,l,r):
+        while l >= 0 and r <len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+    
+    for i in range(len(s)):
+        single_center_palindrome = extend_from_center(s, i, i)
+        double_center_palindrome = extend_from_center(s, i, i+1)
+        
+        if len(single_center_palindrome) > len(longest_palindrome):
+            longest_palindrome = single_center_palindrome
+        if len(double_center_palindrome) > len(longest_palindrome):
+            longest_palindrome = double_center_palindrome
+            
+    if len(longest_palindrome) < 2:
+        return ""
+
+        
+    return longest_palindrome
+
+
+
--- q2.py
+++ q2.py
@@ -1,5 +1,10 @@
 def remove_adjacent_duplicates(s):
-    '''
-    Given a string remove all the adjacent duplicate characters and return the string
-    '''
-    pass
+  my_str = ""
+  
+  for cha in s:
+      my_str += cha
+      
+      if len(my_str) >= 2 and my_str[-1] == my_str[-2]:
+          my_str = my_str[:-2]
+    
+  return my_str
